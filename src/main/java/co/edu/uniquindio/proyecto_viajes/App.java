5c4bb459d829332d987866e723393b0f9a9d378b
Omar <omar.piedrahitat@gmail.com>
2023-11-10 12:47:15 -0500

    Montando estructura de carpetas diviendo cliente y servidor

--- src/main/java/co/edu/uniquindio/proyecto_viajes/App.java
+++ src/main/java/co/edu/uniquindio/proyecto_viajes/App.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("principal-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("client/principal-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Principal");
         stage.setScene(scene);
