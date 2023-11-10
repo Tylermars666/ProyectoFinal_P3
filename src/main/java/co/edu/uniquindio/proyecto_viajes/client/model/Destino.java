e36a8f7ae886b67d782fb1aa51e05479681b2f9a
CarlosMancera <carlosh.mancerar@uqvirtual.edu.co>
2023-11-10 15:50:23 -0500

    Se añade clases

---
+++ src/main/java/co/edu/uniquindio/proyecto_viajes/client/model/Destino.java
@@ -0,0 +1,12 @@
+package co.edu.uniquindio.proyecto_viajes.client.model;
+
+import java.lang.reflect.Array;
+
+public class Destino {
+
+    String nombre;
+    String ciudad;
+    String[] imagenes = new String[]
+    String clima;
+
+}
