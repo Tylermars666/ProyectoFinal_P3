601faa45cf86fa659773ee1251b284b232c0d1b0
Omar <omar.piedrahitat@gmail.com>
2023-11-11 12:14:55 -0500

    Creo ventana de chat para asesor y su respectivo controller

--- src/main/java/co/edu/uniquindio/proyecto_viajes/clienteAsesor/ChatAppAsesor.java
+++ src/main/java/co/edu/uniquindio/proyecto_viajes/clienteAsesor/ChatAppAsesor.java
@@ -1,4 +1,25 @@
 package co.edu.uniquindio.proyecto_viajes.clienteAsesor;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class ChatAppAsesor {
+
+    @FXML
+    private Button btnEnviar;
+
+    @FXML
+    private TextArea txtAreaChat;
+
+    @FXML
+    private TextField txtMensaje;
+
+    @FXML
+    void enviarMensaje(ActionEvent event) {
+
+    }
+
 }
