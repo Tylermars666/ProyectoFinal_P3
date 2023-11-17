f7e935a1ba0a4b995702980d86f3e62dec1911ff
Omar <omar.piedrahitat@gmail.com>
2023-11-17 18:51:01 -0500

    Parte gráfica ventana principal terminada

--- src/main/java/co/edu/uniquindio/proyecto_viajes/client/controller/PrincipalController.java
+++ src/main/java/co/edu/uniquindio/proyecto_viajes/client/controller/PrincipalController.java
@@ -1,4 +1,146 @@
 package co.edu.uniquindio.proyecto_viajes.client.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
 public class PrincipalController {
+
+    @FXML
+    private Button btnAnterior;
+
+    @FXML
+    private Button btnSiguiente;
+
+    @FXML
+    private ComboBox<?> cmbFiltroBusqueda;
+
+    @FXML
+    private TableColumn<?, ?> colCiudad;
+
+    @FXML
+    private TableColumn<?, ?> colClima;
+
+    @FXML
+    private TableColumn<?, ?> colCupoPaquete;
+
+    @FXML
+    private TableColumn<?, ?> colDuracionPaquete;
+
+    @FXML
+    private TableColumn<?, ?> colFechaLimitePaquete;
+
+    @FXML
+    private TableColumn<?, ?> colNombrePaquete;
+
+    @FXML
+    private TableColumn<?, ?> colPrecioPaquete;
+
+    @FXML
+    private ImageView imgImagenCiudad;
+
+    @FXML
+    private ImageView imgLogoPrincipal;
+
+    @FXML
+    private Label lblDescripcion;
+
+    @FXML
+    private TableView<?> tblCiudad;
+
+    @FXML
+    private TableView<?> tblPaquetes;
+
+    @FXML
+    private TextArea txtADescripcionDestino;
+
+    @FXML
+    private TextField txtBusqueda;
+
+    @FXML
+    void abrirChat(ActionEvent event) throws Exception{
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/co/edu/uniquindio/proyecto_viajes/client/view/chat-cliente-view.fxml"));
+
+        Scene scene = new Scene(loader.load());
+        Stage stage = new Stage();
+        stage.setTitle("Chat");
+        stage.setScene(scene);
+
+        ChatClienteController controller = loader.getController();
+        stage.show();
+
+    }
+
+    @FXML
+    void abrirEdicionPerfil(ActionEvent event) throws Exception{
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/co/edu/uniquindio/proyecto_viajes/client/view/edicion-view.fxml"));
+
+        Scene scene = new Scene(loader.load());
+        Stage stage = new Stage();
+        stage.setTitle("Edicion");
+        stage.setScene(scene);
+
+        ChatClienteController controller = loader.getController();
+        stage.show();
+
+    }
+
+    @FXML
+    void abrirVentanaReserva(ActionEvent event) throws Exception {
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/co/edu/uniquindio/proyecto_viajes/client/view/reserva-view.fxml"));
+
+        Scene scene = new Scene(loader.load());
+        Stage stage = new Stage();
+        stage.setTitle("Reserva");
+        stage.setScene(scene);
+
+        ChatClienteController controller = loader.getController();
+        stage.show();
+
+    }
+
+    @FXML
+    void buscar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void seleccionarCiudad(MouseEvent event) {
+
+    }
+
+    @FXML
+    void seleccionarDestino(MouseEvent event) {
+
+    }
+
+    @FXML
+    void seleccionarFiltro(ActionEvent event) {
+
+    }
+
+    @FXML
+    void verAnteriorImagen(ActionEvent event) {
+
+    }
+
+    @FXML
+    void verSiguienteImagen(ActionEvent event) {
+
+    }
+
 }
