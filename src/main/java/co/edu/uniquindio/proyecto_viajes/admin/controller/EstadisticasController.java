9322a4cb77e92884c40d25935a478b3b3c471cf7
Omar <omar.piedrahitat@gmail.com>
2023-11-19 17:47:01 -0500

    estadisticas admin terminadas

--- src/main/java/co/edu/uniquindio/proyecto_viajes/admin/controller/EstadisticasController.java
+++ src/main/java/co/edu/uniquindio/proyecto_viajes/admin/controller/EstadisticasController.java
@@ -1,4 +1,11 @@
 package co.edu.uniquindio.proyecto_viajes.admin.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.chart.StackedBarChart;
+
 public class EstadisticasController {
+
+    @FXML
+    private StackedBarChart<?, ?> grafica;
+
 }
