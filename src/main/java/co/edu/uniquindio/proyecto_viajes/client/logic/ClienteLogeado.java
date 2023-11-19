c8db0eff7feed27cf517d3184ce36fedbc7cb771
Omar <omar.piedrahitat@gmail.com>
2023-11-19 14:38:58 -0500

    Creo cliente logeado

---
+++ src/main/java/co/edu/uniquindio/proyecto_viajes/client/logic/ClienteLogeado.java
@@ -0,0 +1,11 @@
+package co.edu.uniquindio.proyecto_viajes.client.logic;
+
+import co.edu.uniquindio.proyecto_viajes.client.model.Cliente;
+import lombok.*;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@AllArgsConstructor
+public class ClienteLogeado extends Cliente {
+
+}
