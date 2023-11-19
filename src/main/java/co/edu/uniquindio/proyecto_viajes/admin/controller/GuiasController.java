c3e790579af583afdadf70b012357b6f05894746
Omar <omar.piedrahitat@gmail.com>
2023-11-19 16:55:33 -0500

    Guias controller de administrado

--- src/main/java/co/edu/uniquindio/proyecto_viajes/admin/controller/GuiasController.java
+++ src/main/java/co/edu/uniquindio/proyecto_viajes/admin/controller/GuiasController.java
@@ -1,4 +1,62 @@
 package co.edu.uniquindio.proyecto_viajes.admin.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
 public class GuiasController {
+
+    @FXML
+    private TableColumn<?, ?> colExperienciaGuia;
+
+    @FXML
+    private TableColumn<?, ?> colIdentificacionGuia;
+
+    @FXML
+    private TableColumn<?, ?> colNombreGuia;
+
+    @FXML
+    private TableView<?> tblGuias;
+
+    @FXML
+    private TextField txtExperienciaGuia;
+
+    @FXML
+    private TextField txtIdentificacionGuia;
+
+    @FXML
+    private TextField txtIdioma1Guia;
+
+    @FXML
+    private TextField txtIdioma2Guia;
+
+    @FXML
+    private TextField txtIdioma3Guia;
+
+    @FXML
+    private TextField txtNombreGuia;
+
+    @FXML
+    void agregarGuia(ActionEvent event) {
+
+    }
+
+    @FXML
+    void editarGuia(ActionEvent event) {
+
+    }
+
+    @FXML
+    void eliminarGuia(ActionEvent event) {
+
+    }
+
+    @FXML
+    void seleccionarGuia(MouseEvent event) {
+
+    }
+
 }
