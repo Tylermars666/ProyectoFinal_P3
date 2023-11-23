7308f63bfb00eaa10071a3fc53e59f3045a7b3f3
Omar <omar.piedrahitat@gmail.com>
2023-11-23 01:08:55 -0500

    Paquetes Controller

--- src/main/java/co/edu/uniquindio/proyecto_viajes/admin/controller/PaquetesController.java
+++ src/main/java/co/edu/uniquindio/proyecto_viajes/admin/controller/PaquetesController.java
@@ -1,15 +1,30 @@
 package co.edu.uniquindio.proyecto_viajes.admin.controller;
 
+import co.edu.uniquindio.proyecto_viajes.DataPaquete;
+import co.edu.uniquindio.proyecto_viajes.client.model.Destino;
+import co.edu.uniquindio.proyecto_viajes.client.model.Paquete;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 
-public class PaquetesController {
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+public class PaquetesController implements Initializable {
 
     @FXML
     private TableColumn<?, ?> colCupoPaquete;
@@ -27,10 +42,28 @@ public class PaquetesController {
     private TableColumn<?, ?> colPrecioPaquete;
 
     @FXML
+    private TableColumn<?, ?> colCiudadDisponible;
+
+    @FXML
+    private TableColumn<?, ?> colCiudadSeleccionada;
+
+    @FXML
+    private TableColumn<?, ?> colClimaDisponible;
+
+    @FXML
+    private TableColumn<?, ?> colClimaSeleccionado;
+
+    @FXML
     private DatePicker dateFechaPaquete;
 
     @FXML
-    private TableView<?> tblPaquetes;
+    private ListView<String> listDestinosDisponibles;
+
+    @FXML
+    private ListView<String> listDestinosSeleccionados;
+
+    @FXML
+    private TableView<Paquete> tblPaquetes;
 
     @FXML
     private TextField txtCupoPaquete;
@@ -46,6 +79,24 @@ public class PaquetesController {
 
     @FXML
     private TextArea txtServiciosAdicionales;
+
+    private ArrayList<String> elementosElegidos;
+
+    @FXML
+    private TableView<Destino> tblDestinosDisponible;
+
+    @FXML
+    private TableView<?> tblDestinosSeleccionados;
+
+    ArrayList<Paquete> paquetesActualizados;
+
+    ArrayList<Destino> destinosActualizados;
+
+    ObservableList<Paquete> paquetesObservables;
+
+    ObservableList<Destino> destinosObservables;
+
+
 
     @FXML
     void agregarPaquete(ActionEvent event) {
@@ -72,4 +123,93 @@ public class PaquetesController {
 
     }
 
+    @FXML
+    void seleccionarDestino(MouseEvent event) {
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        updateList();
+        updateListDestinos();
+
+    }
+
+    public void updateList(){
+        try{
+
+            Socket socket = new Socket("localhost",9595);
+
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+            DataPaquete dataPaquete = new DataPaquete("paquete","listar",null);
+            objectOutputStream.writeObject(dataPaquete);
+
+            ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
+
+            ArrayList<Paquete> paqueteRecibido = (ArrayList<Paquete>) objectInputStream.readObject();
+
+            objectOutputStream.close();
+            objectInputStream.close();
+            socket.close();
+
+
+
+            this.paquetesActualizados = paqueteRecibido;
+
+            paquetesObservables = FXCollections.observableArrayList();
+            this.colNombrePaquete.setCellValueFactory(new PropertyValueFactory<>("nombre"));
+            this.colDuracionPaquete.setCellValueFactory(new PropertyValueFactory<>("duracion"));
+            this.colPrecioPaquete.setCellValueFactory(new PropertyValueFactory<>("precio"));
+            this.colCupoPaquete.setCellValueFactory(new PropertyValueFactory<>("cupo"));
+            this.colFechaPaquete.setCellValueFactory(new PropertyValueFactory<>("fecha"));
+
+
+            this.paquetesObservables.addAll(this.paquetesActualizados);
+            this.tblPaquetes.setItems(paquetesObservables);
+
+
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void updateListDestinos(){
+
+        try{
+
+            Socket socket = new Socket("localhost",9595);
+
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+            DataPaquete dataPaquete = new DataPaquete("destino","listar",null);
+            objectOutputStream.writeObject(dataPaquete);
+
+            ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
+
+            ArrayList<Destino> destinoRecibido = (ArrayList<Destino>) objectInputStream.readObject();
+
+            objectOutputStream.close();
+            objectInputStream.close();
+            socket.close();
+
+            this.destinosActualizados = destinoRecibido;
+
+            destinosObservables = FXCollections.observableArrayList();
+            this.colCiudadDisponible.setCellValueFactory(new PropertyValueFactory<>("ciudad"));
+            this.colClimaDisponible.setCellValueFactory(new PropertyValueFactory<>("clima"));
+
+            this.destinosObservables.addAll(this.destinosActualizados);
+            this.tblDestinosDisponible.setItems(destinosObservables);
+
+
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+    }
 }
